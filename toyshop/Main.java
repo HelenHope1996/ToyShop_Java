f0083d5985e200711ee0575c0f1264df8c200851
Helen <lenapukhova3m96@mail.ru>
2023-05-10 00:09:58 +0300

    add file main

---
+++ toyshop/Main.java
@@ -0,0 +1,31 @@
+package toyshop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Toy toy1 = new Toy(00, 4, "Вертолет", 10);
+        Toy toy2 = new Toy(01, 8,  "Машинка", 50);
+        Toy toy3 = new Toy(02, 1,  "Кукла", 8);
+        Toy toy4 = new Toy(03, 5, "Пазлы", 23);
+
+        List<Toy> toys = new ArrayList<Toy>();
+        toys.add(toy4);
+        toys.add(toy3);
+        toys.add(toy2);
+        toys.add(toy1);
+
+        ToyShop toyStore = new ToyShop(toys);
+        toyStore.saveToyForLottery();
+        toyStore.saveToyForLottery();
+        toyStore.saveToyForLottery();
+        toyStore.saveToyForLottery();
+
+
+    }
+    
+    
+}
