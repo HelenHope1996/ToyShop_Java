5f03f19c2dd3de5d38dc0c92fffe083625dbc574
Helen <lenapukhova3m96@mail.ru>
2023-05-10 00:17:03 +0300

    edit file toyshop

--- toyshop/ToyShop.java
+++ toyshop/ToyShop.java
@@ -20,7 +20,7 @@ public class ToyShop {
     public void saveToyForLottery() {
         Toy toy = getToyForPrice();
         String text = toy.toString();
-        try(FileWriter writer = new FileWriter("toy-shop/ToyShop/Toys.txt", true))
+        try(FileWriter writer = new FileWriter("Shop/toyshop/Toys.txt", true))
         { 
             writer.write(text);
             writer.append('\n');
